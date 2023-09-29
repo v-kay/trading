1b5ac56223abde4789ed8118dcc78a5762582988
v-kay <47169706+v-kay@users.noreply.github.com>
2023-09-29 17:04:31 +0800

    Create WarriorsAgent.java

---
+++ src/main/java/com/vmware/tanzu/demos/sta/tradingagent/bid/WarriorsAgent.java
@@ -0,0 +1,35 @@
+package com.vmware.tanzu.demos.sta.tradingagent.bid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Component
+@ConditionalOnProperty(name = "app.agent.strategy", havingValue = "pas")
+class WarriorsAgent implements BidAgent {
+    private final Logger logger = LoggerFactory.getLogger(WarriorsAgent.class);
+
+    @Override
+    public List<BidAgentRequest> execute(Context ctx) {
+        // Sort input stocks against price.
+        final List<Stock> sortedStocks = new ArrayList<>(ctx.stocks());
+        sortedStocks.sort(Comparator.comparing(Stock::price));
+
+        final Stock lowerStock = sortedStocks.get(0);
+        final Stock higherStock = sortedStocks.get(sortedStocks.size()-1);
+
+        logger.info("Found a stock with the lower value: {}", lowerStock.symbol());
+        return List.of(new BidAgentRequest(lowerStock.symbol(), 100)
+                ,new BidAgentRequest(higherStock.symbol(), -100));
+    }
+
+    @Override
+    public String toString() {
+        return "BUY_LOWER_STOCK";
+    }
+}
